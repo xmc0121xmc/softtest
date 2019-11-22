a73050b544c72de8c86caf272dba10eec8c89cf2
xmc0121xmc <1095921497@qq.com>
2019-11-22 10:13:46 +0800

    add

---
+++ unittest11.py
@@ -0,0 +1,20 @@
+import requests
+import unittest
+class Testadd(unittest.TestCase):
+    def setUp(self):
+        print("start")
+    def tearDown(self):
+        print("stop")
+
+    def test_01(self):
+        a = 1
+        b = 1
+        self.assertEqual(2, a+b)
+
+    def test_02(self):
+        a = 1
+        b = 1
+        self.assertEqual(3, a+b)
+
+if __name__ == '__main__':
+    unittest.main()
